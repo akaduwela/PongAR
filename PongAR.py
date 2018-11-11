34c1f671a38dcde82bbe67d7c5f81699561f23f0
refai1 <refai241@gmail.com>
2018-11-10 17:26:58 -0800

    keyboard functionality

--- PongAR.py
+++ PongAR.py
@@ -49,22 +49,21 @@ class Ball(pygame.sprite.Sprite):
         self.reset()
  
     def reset(self):
-        self.x = random.randrange(50,750)
-        self.y = 350.0
+        self.y = random.randrange(50,550)
+        self.x = 400.0
         self.speed=8.0
  
         # Direction of ball (in degrees)
-        self.direction = random.randrange(-45,45)
- 
+        self.direction = random.randrange(45,135)
+
         # Flip a 'coin'
         if random.randrange(2) == 0 :
             # Reverse ball direction, let the other guy get it first
-            self.direction += 180
-            self.y = 50
+            self.direction -= 180
  
     # This function will bounce the ball off a horizontal surface (not a vertical one)
     def bounce(self,diff):
-        self.direction = (180-self.direction)%360
+        self.direction = (360-self.direction)%360
         self.direction -= diff
  
         # Speed the ball up
@@ -79,60 +78,83 @@ class Ball(pygame.sprite.Sprite):
         self.x += self.speed * math.sin(direction_radians)
         self.y -= self.speed * math.cos(direction_radians)
  
-        if self.y < 0:
+        if self.x < 0:
             self.reset()
  
-        if self.y > 600:
+        if self.x > 800:
             self.reset()
  
         # Move the image to where our x and y are
         self.rect.x = self.x
         self.rect.y = self.y
  
-        # Do we bounce off the left of the screen?
-        if self.x <= 0:
-            self.direction = (360-self.direction)%360
+        # Do we bounce off the top of the screen?
+        if self.y <= 0:
+            self.direction = (180-self.direction)%360
             print(self.direction)
             #self.x=1
  
-        # Do we bounce of the right side of the screen?
-        if self.x > self.screenwidth-self.width:
-            self.direction = (360-self.direction)%360
+        # Do we bounce of the bottom of the screen?
+        if self.y > self.screenheight-self.height:
+            self.direction = (180-self.direction)%360
  
 # This class represents the bar at the bottom that the player controls
 class Player(pygame.sprite.Sprite):
     # Constructor function
-    def __init__(self, joystick, y_pos):
+    def __init__(self,pid, y_pos):
         # Call the parent's constructor
         super().__init__()
  
-        self.width=75
-        self.height=15
+        self.width=15
+        self.height=75
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(WHITE)
-        self.joystick = joystick
  
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.screenheight = pygame.display.get_surface().get_height()
         self.screenwidth = pygame.display.get_surface().get_width()
  
-        self.rect.x = 0
-        self.rect.y = y_pos
- 
+        self.rect.y = 0
+        self.rect.x = y_pos
+        self.pid = pid
+
     # Update the player
     def update(self):
- 
-        # This gets the position of the axis on the game controller
-        # It returns a number between -1.0 and +1.0
-        horiz_axis_pos= self.joystick.get_axis(0)
- 
-        # Move x according to the axis. We multiply by 15 to speed up the movement.
-        self.rect.x=int(self.rect.x+horiz_axis_pos*15)
- 
+
+        if (event.type == pygame.KEYDOWN):
+            if (event.key == ord('q')):
+                pygame.quit()
+     
+        horiz_axis_pos = 0
+
+        if (self.pid == 0):
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_UP):
+                    horiz_axis_pos = -1
+
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_DOWN):
+                    horiz_axis_pos = 1
+            
+            # Move x according to the axis. We multiply by 15 to speed up the movement.
+            self.rect.y=int(self.rect.y+horiz_axis_pos*15)
+        elif(self.pid == 1):
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == ord('w')):
+                    horiz_axis_pos = -1
+
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == ord('s')):
+                    horiz_axis_pos = 1
+            
+            # Move x according to the axis. We multiply by 15 to speed up the movement.
+            self.rect.y=int(self.rect.y+horiz_axis_pos*15)
+
+
         # Make sure we don't push the player paddle off the right side of the screen
-        if self.rect.x > self.screenwidth - self.width:
-            self.rect.x = self.screenwidth - self.width
+        if self.rect.y > self.screenheight - self.height:
+            self.rect.y = self.screenheight - self.height
  
 score1 = 0
 score2 = 0
@@ -161,23 +183,11 @@ ball = Ball()
 balls = pygame.sprite.Group()
 balls.add(ball)
  
-# Count the joysticks the computer has
-joystick_count = pygame.joystick.get_count()
-if joystick_count < 1:
-    # No joysticks!
-    print ("Error, I didn't find enough joysticks.")
-    pygame.quit()
-    exit()
-else:
-    # Use joystick #0 and initialize it
-    joystick1 = pygame.joystick.Joystick(0)
-    joystick1.init()
-    joystick2 = pygame.joystick.Joystick(1)
-    joystick2.init()
- 
+
+
 # Create the player paddle object
-player1 = Player(joystick1,580)
-player2 = Player(joystick2,25)
+player1 = Player(0, 780)
+player2 = Player(1, 25)
  
 movingsprites = pygame.sprite.Group()
 movingsprites.add(player1)
@@ -217,20 +227,20 @@ while not exit_program:
     # See if the ball hits the player paddle
     if pygame.sprite.spritecollide(player1, balls, False):
         # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
-        diff = (player1.rect.x + player1.width/2) - (ball.rect.x+ball.width/2)
+        diff = (player1.rect.y + player1.height/2) - (ball.rect.y+ball.height/2)
  
         # Set the ball's y position in case we hit the ball on the edge of the paddle
-        ball.y = 570
-        ball.bounce(diff)
+        ball.x = 770
+        ball.bounce(-1*diff)
         score1 += 1
  
     # See if the ball hits the player paddle
     if pygame.sprite.spritecollide(player2, balls, False):
         # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
-        diff = (player2.rect.x + player2.width/2) - (ball.rect.x+ball.width/2)
+        diff = (player2.rect.y + player2.height/2) - (ball.rect.y+ball.height/2)
  
         # Set the ball's y position in case we hit the ball on the edge of the paddle
-        ball.y = 40
+        ball.x = 40
         ball.bounce(diff)
         score2 += 1
  
